75d4ccfd459ff7e94954d2c8f2214e1c8abc17ae
berlysia <berlysia@gmail.com>
2023-11-24 02:51:17 +0900

    remove unused deps

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import withMDX from "@next/mdx";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
@@ -11,4 +9,4 @@ const nextConfig = {
   },
 };
 
-export default withMDX()(nextConfig);
+export default nextConfig;
