40aea3b2bbd10cdfd8823b503b7308408e6556e6
berlysia <berlysia@gmail.com>
2024-01-14 15:25:27 +0900

    fix hatena bookmark counter

--- src/components/pages/root/HatenaBookmarkCounter.tsx
+++ src/components/pages/root/HatenaBookmarkCounter.tsx
@@ -7,10 +7,17 @@ export default function HatenaBookmarkCounter({
   readonly alt?: string;
 }) {
   return (
-    <img
-      className="tw-block tw-object-contain"
-      src={`https://b.hatena.ne.jp/entry/image/${link}`}
-      alt={alt ?? "はてなブックマーク"}
-    />
+    <a
+      href={`https://b.hatena.ne.jp/entry/${link}`}
+      className="tw-flex tw-items-center tw-rounded-md tw-px-1 tw-h-6 tw-flex-shrink-0 tw-text-blue-600 visited:tw-text-purple-800 hover:tw-bg-gray-200 focus:tw-bg-gray-200"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        className="tw-block tw-object-contain"
+        src={`https://b.hatena.ne.jp/entry/image/${link}`}
+        alt={alt ?? "はてなブックマーク"}
+      />
+    </a>
   );
 }
